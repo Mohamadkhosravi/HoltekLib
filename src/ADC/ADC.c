ddfe6128313cf39d893d0364c391ebdc7e2f5947
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2024-08-23 12:48:01 +0330

    Add necessary files and directories

---
+++ src/ADC/ADC.c
@@ -0,0 +1,118 @@
+/*
+ * Licensed under the Apache License, Version 2.0.
+ * You may not use this file except in compliance with the License.
+ * Obtain a copy at http://www.apache.org/licenses/LICENSE-2.0.
+ * Distributed on an "AS IS" basis, without warranties or conditions.
+ */
+
+/** 
+ * @file ADC.c
+ * @brief Implementation of ADC configuration and reading functions.
+ * 
+ * This file contains the implementation of functions to initialize the ADC
+ * and read analog values from specified channels on the BA45F5240 microcontroller.
+ * 
+ * Author: Mohamad Khosravi  https://github.com/Mohamadkhosravi
+ * Date: 2024
+ */
+
+#include "ADC.h"
+
+/**
+ * @brief Initialize the ADC module.
+ * 
+ * This function initializes the ADC module by setting up the control registers.
+ * It prepares the ADC for reading from various channels.
+ */
+void ADCInit()
+{
+    // Configure ADC control registers
+    _sadc0 = 0b00000111;
+    _sadc1 = 0b01101011;
+	_adrfs = ADC_RESOLUTION
+    _ade = 0; // Disable analog interrupt initially
+}
+
+/**
+ * @brief Read analog value from a specified ADC channel.
+ * 
+ * This function reads the analog value from the specified ADC channel and returns 
+ * the digital equivalent.
+ * 
+ * @param channel The ADC channel to read from (AN0, AN1, AN2, AN3, VBGREF, OPA0O, OPA1O, LINEV).
+ * @return The digital value corresponding to the analog input.
+ */
+unsigned int ReadADC(unsigned char channel)
+{
+    unsigned int result;
+    
+    switch(channel)
+    {
+#if USE_ADC_AN0
+        case AN0:
+            _RD_ADC_SET_AN0;
+            break;
+#endif
+#if USE_ADC_AN1
+        case AN1:
+            _RD_ADC_SET_AN1;
+            break;
+#endif
+#if USE_ADC_AN2
+        case AN2:
+            _RD_ADC_SET_AN2;
+            break;
+#endif
+#if USE_ADC_AN3
+        case AN3:
+            _RD_ADC_SET_AN3;
+            break;
+#endif
+#if USE_ADC_VBGREF
+        case VBGREF:
+            _RD_ADC_SET_VBGREF;
+            break;
+#endif
+#if USE_ADC_OPA0O
+        case OPA0O:
+            _RD_ADC_SET_OPA0O;
+            break;
+#endif
+#if USE_ADC_OPA1O
+        case OPA1O:
+            _RD_ADC_SET_OPA1O;
+            break;
+#endif
+#if USE_ADC_LINEV
+        case LINEV:
+            _RD_ADC_SET_LINEV;
+            break;
+#endif
+        default:
+            return 0; // Invalid channel
+    }
+    
+    // Start ADC conversion
+    ADC_ON;
+    GCC_DELAY(12); // Small delay to stabilize the signal
+    _start = 0;    // Clear start bit
+    _start = 1;    // Set start bit to begin conversion
+    _start = 0;    // Clear start bit again
+    
+    // Wait for conversion to complete
+    while (_adbz);
+    
+    // Disable ADC after conversion
+    ADC_OFF;
+    
+    // Read result based on resolution
+    if (_adrfs == 0) {
+        result = _sadoh;
+        result = (result << 8) | _sadol;
+    } else {
+        result = (_sadoh & 0x0F); // Only lower 4 bits are valid
+        result = (result << 8) | _sadol;
+    }
+
+    return result; // Return the digital value
+}
---
+++ src/ADC/ADC.h
@@ -0,0 +1,96 @@
+/*
+ * Licensed under the Apache License, Version 2.0.
+ * You may not use this file except in compliance with the License.
+ * Obtain a copy at http://www.apache.org/licenses/LICENSE-2.0.
+ * Distributed on an "AS IS" basis, without warranties or conditions.
+ */
+
+/** 
+ * @file ADC.h
+ * @brief Header file for ADC configuration and operations.
+ * 
+ * This file contains macros and function prototypes for initializing 
+ * and reading from the ADC channels on the BA45F5240 microcontroller.
+ * 
+ * Author: Mohamad Khosravi  https://github.com/Mohamadkhosravi
+ * Date: 2024
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include "BA45F5240.h"
+
+
+
+// Common ADC configuration macros
+#define Disable 0
+#define Enable  1
+// ADC Resolution Settings
+#define RESOLUTION_12BIT 0
+#define RESOLUTION_8BIT  1
+
+#define ADC_RESOLUTION  RESOLUTION_12BIT
+
+// Macros to enable and disable the ADC
+#define ADC_ON   _adcen = 1; 
+#define ADC_OFF  _adcen = 0; 
+
+// Enable or Disable specific ADC channels
+#define USE_ADC_AN0      Enable
+#define USE_ADC_AN1      Enable
+#define USE_ADC_AN2      Enable
+#define USE_ADC_AN3      Enable
+#define USE_ADC_VBGREF   Disable
+#define USE_ADC_OPA0O    Disable
+#define USE_ADC_OPA1O    Disable
+#define USE_ADC_LINEV    Disable
+
+
+// Define ADC channels based on usage
+#if USE_ADC_AN0
+#define AN0 0x00
+#define READ_ADC_SET_AN0 { _sadc1 = 0b00000011; _sadc0 = 0b00000000; }
+#endif
+
+#if USE_ADC_AN1
+#define AN1 0x01
+#define READ_ADC_SET_AN1 { _sadc1 = 0b00001000; _sadc0 = 0b00000001; }
+#endif
+
+#if USE_ADC_AN2
+#define AN2 0x02
+#define READ_ADC_SET_AN2 { _sadc1 = 0b00000011; _sadc0 = 0b00000010; }
+#endif
+
+#if USE_ADC_AN3
+#define AN3 0x03
+#define READ_ADC_SET_AN3 { _sadc1 = 0b00000011; _sadc0 = 0b00000011; }
+#endif
+
+#if USE_ADC_VBGREF
+#define VBGREF 0x04
+#define READ_ADC_SET_VBGREF { _sadc1 = 0b00101011; _sadc0 = 0b00010111; }
+#endif
+
+#if USE_ADC_OPA0O
+#define OPA0O 0x05
+#define READ_ADC_SET_OPA0O { _sadc1 = 0b01000011; _sadc0 = 0b00000111; }
+#endif
+
+#if USE_ADC_OPA1O
+#define OPA1O 0x06
+#define READ_ADC_SET_OPA1O { _sadc1 = 0b01101011; _sadc0 = 0b00000111; }
+#endif
+
+#if USE_ADC_LINEV
+#define LINEV 0x07
+#define READ_ADC_SET_LINEV { _sadc1 = 0b10000011; _sadc0 = 0b00000111; }
+#endif
+
+
+// Function prototypes
+void ADCInit();
+unsigned int ReadADC(unsigned char channel);
+
+#endif // ADC_H_
