217f2f9d781d3b54b5e15c6c1c4a49e0a16f225f
Mohammad <khosravimmd@gmail.com>
2025-12-19 01:24:23 +0330

    FEAT:add control-flow macros and initial non-blocking process structure

--- src/OneWire/OneWire.h
+++ src/OneWire/OneWire.h
@@ -18,13 +18,16 @@
 #ifndef _ONE_WIRE_H_ 
 #define _ONE_WIRE_H_
 
-
-/* Includes-------------------------------------------------------------------------------------------------*/
-
+/* ================= Includes =================*/
 #include <stdint.h>
 
+/* ================= Compile-time control ================= */
+#define ONEWIRE_DELAY_BASED_ENABLE      1
+#define ONEWIRE_NON_BLOCKING_ENABLE  0   /* default OFF */
 
-/* Exported functions---------------------------------------------------------------------------------------*/
+
+
+/* ================= Exported functions =================*/
 
 #define pin_data        _pa3
 #define pin_mode        _pac3
@@ -34,6 +37,8 @@
 
 #define pin_high   pin_data=1;
 #define pin_low    pin_data=0;
+/* ================= Delay-Based API ========================= */
+#if ONEWIRE_DELAY_BASED_ENABLE
 
 void delay_four_micro(unsigned long int number_of_seconds);
 bool initiate(void);
@@ -42,5 +47,19 @@ void write_one(void);
 void write_zero(void);
 void OneWire_write_byte(uint8_t Tdata);
 uint8_t OneWire_read_byte(void);
+#endif
+/* ================= Non-blocking control ================= */
+#if ONEWIRE_NON_BLOCKING_ENABLE
+
+typedef union __attribute__((packed)) {
+    struct {
+        uint8_t busy     : 1;
+        uint8_t presence : 1;
+        uint8_t done     : 1;
+        uint8_t reserved : 5;
+    };
+    uint8_t all;
+} OneWireFlags_t;
+#endif
 
 #endif
